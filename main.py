d2679b29c5753515c746e18a1ab2d01879483928
alenmak <siatfamg@gmail.com>
2025-04-18 23:04:55 +0500

    база

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from env import TG_TOKEN  # импортируем токен
-import logging
+from env import TG_TOKEN  # импорт токена
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 
-# Запускаем логгирование
+reply_keyboard = [[KeyboardButton(text='/address'), KeyboardButton(text='/phone')],
+                  [KeyboardButton(text='/site'), KeyboardButton(text='/work_time')],
+                  [KeyboardButton(text="/stop")]]
+kb = ReplyKeyboardMarkup(keyboard=reply_keyboard, resize_keyboard=True, one_time_keyboard=False)
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
-# создаем диспетчер
+# создаем маршрутизатор
 dp = Dispatcher()
 
 
@@ -18,18 +23,39 @@ async def main():
     await dp.start_polling(bot)
 
 
-@dp.message(Command('start'))  # декоратор для обработчика команды start
-async def process_start_command(message: types.Message):
-    """
-    Создаем и регистрируем в диспетчере асинхронный обработчик сообщений.
-    В параметре message содержится вся информация о сообщении - см. документацию.
-    """
-    await message.reply("Привет!\nНапиши мне что-нибудь!")  # отправляет ответ на сообщение
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    await message.reply("Привет.", reply_markup=kb)
 
 
-@dp.message(Command('help'))  # декоратор для обработчика команды help
-async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отправлю этот текст тебе в ответ!")
+@dp.message(Command('stop'))
+async def stop(message: types.Message):
+    await message.reply("Пока.", reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message(Command('help'))
+async def help(message: types.Message):
+    await message.reply("Я бот справочник.")
+
+
+@dp.message(Command('address'))
+async def address(message: types.Message):
+    await message.reply("Адрес: г. Москва, ул. Льва Толстого, 16")
+
+
+@dp.message(Command('phone'))
+async def phone(message: types.Message):
+    await message.reply("Телефон: +7(495)776-3030")
+
+
+@dp.message(Command('site'))
+async def site(message: types.Message):
+    await message.reply("Сайт: http://www.yandex.ru/company")
+
+
+@dp.message(Command('work_time'))
+async def work_time(message: types.Message):
+    await message.reply("Время работы: круглосуточно.")
 
 
 @dp.message()  # декоратор для обработчика прочих сообщений
